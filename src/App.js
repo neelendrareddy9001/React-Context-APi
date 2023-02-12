057e116cf60c0726b01822771f52144ca5e2420c
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-02-12 23:54:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react;
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
@@ -6,12 +7,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
+  
+  const [cart, setCart] = useState([]);
   return (
       <Router>
         <Header/>
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/' element={<Home cart={cart} setCart={setCart}/>}/>
+          <Route path='/cart' element={<Cart cart={cart} setCart={setCart}/>}/>
         </Routes>
       </Router>
   );
